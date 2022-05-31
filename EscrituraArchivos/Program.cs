3d48acc50a2c295e5689df582435fa14ca6ba0da
JNoriegaH <jorgeivan0317@gmail.com>
2022-05-31 11:57:27 -0700

    Captura de Nombres

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -11,22 +11,17 @@ namespace EscrituraArchivos
     {
         static void Main(string[] args)
         {
+            string name;
             StreamWriter sw = new StreamWriter("ejemplo.txt",true);
             //si el archivo no existe lo creará
             //si ya existe, escribirá en él
             //true es para agregar y no sobreescribir
 
-            string[] lines =
-            {
-                "Esta es la nueva escritura",
-                "Esta es la segunda linea",
-                "Fin del texto"
-            };
-            //recorrer el arreglo
-            foreach(string line in lines)
-            {
-                sw.WriteLine(line);//escribe en el archivo
-            }
+            Console.Write("Ingrese un nombre: ");
+            name = Console.ReadLine();
+
+            sw.WriteLine(name);//escribe en el archivo
+
             sw.Close();//se debe cerrar el archivo
 
             Console.WriteLine("Escribiendo en el archivo...");
