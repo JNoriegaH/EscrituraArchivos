7932c699d03cc2036268559ff58386b87bfef2e9
JNoriegaH <jorgeivan0317@gmail.com>
2022-06-01 16:18:30 -0700

    ProgramaTarea

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -7,20 +7,59 @@ using System.IO;
 
 namespace EscrituraArchivos
 {
+    class Cliente //Creacion de la clase Cliente
+    {
+        //Atributos
+        string nombreCliente;
+        int numeroCliente;
+        string direccion;
+        long telefono;
+
+        //Constructor
+        public Cliente(string nombreCliente, int numeroCliente, string direccion, long telefono)
+        {
+            //recibe los parámetros con mismos nombres y los asigna a los campos de la clase con this.
+            this.nombreCliente = nombreCliente;
+            this.numeroCliente = numeroCliente;
+            this.direccion = direccion;
+            this.telefono = telefono;
+        }
+        //Metodo funcion que calcula la venta
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string name;
+            //Declaracion de variables
+            string nombreCliente, direccion;
+            int numeroCliente;
+            long telefono;
+
             StreamWriter sw = new StreamWriter("ejemplo.txt",true);
             //si el archivo no existe lo creará
             //si ya existe, escribirá en él
             //true es para agregar y no sobreescribir
 
-            Console.Write("Ingrese un nombre: ");
-            name = Console.ReadLine();
+            //Captura de datos
+            Console.Write("Ingrese su nombre:");
+            nombreCliente = Console.ReadLine();
+            Console.Write("Ingrese numero de cliente:");
+            numeroCliente = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese su direccion:");
+            direccion = Console.ReadLine();
+            Console.Write("Ingrese su telefono:");
+            telefono = long.Parse(Console.ReadLine());
 
-            sw.WriteLine(name);//escribe en el archivo
+            //Se crea el objeto de la clase cliente enviándole los datos como parámetros al constructor.
+            Cliente comprador = new Cliente(nombreCliente, numeroCliente, direccion, telefono);
+
+            //Escribe en el archivo
+            sw.WriteLine("Nombre del cliente:          {0}", nombreCliente);
+            sw.WriteLine("Numero del cliente:          {0}", numeroCliente);
+            sw.WriteLine("Direccion:                   {0}", direccion);
+            sw.WriteLine("Telefono:                    {0}", telefono);
+
+
 
             sw.Close();//se debe cerrar el archivo
 
